4172f1d5175598593e30ab92cb26f45ed6a2144c
shawnyeh0308 <92806848+shawnyeh0308@users.noreply.github.com>
2022-07-21 23:31:54 +0800

    Add files via upload

---
+++ CameraFaceRecognitionDoorLock/AI_THINKER.h
@@ -0,0 +1,70 @@
+#if defined(CAMERA_MODEL_AI_THINKER)
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+#else
+#error "Camera model not selected"
+#endif
+
+void configSetup()
+{
+camera_config_t config;
+config.ledc_channel = LEDC_CHANNEL_0;
+config.ledc_timer = LEDC_TIMER_0;
+config.pin_d0 = Y2_GPIO_NUM;
+config.pin_d1 = Y3_GPIO_NUM;
+config.pin_d2 = Y4_GPIO_NUM;
+config.pin_d3 = Y5_GPIO_NUM;
+config.pin_d4 = Y6_GPIO_NUM;
+config.pin_d5 = Y7_GPIO_NUM;
+config.pin_d6 = Y8_GPIO_NUM;
+config.pin_d7 = Y9_GPIO_NUM;
+config.pin_xclk = XCLK_GPIO_NUM;
+config.pin_pclk = PCLK_GPIO_NUM;
+config.pin_vsync = VSYNC_GPIO_NUM;
+config.pin_href = HREF_GPIO_NUM;
+config.pin_sscb_sda = SIOD_GPIO_NUM;
+config.pin_sscb_scl = SIOC_GPIO_NUM;
+config.pin_pwdn = PWDN_GPIO_NUM;
+config.pin_reset = RESET_GPIO_NUM;
+config.xclk_freq_hz = 20000000;
+config.pixel_format = PIXFORMAT_JPEG;
+  
+//
+// WARNING!!! PSRAM IC required for UXGA resolution and high JPEG quality
+//            Ensure ESP32 Wrover Module or other board with PSRAM is selected
+//            Partial images will be transmitted if image exceeds buffer size
+//   
+// if PSRAM IC present, init with UXGA resolution and higher JPEG quality
+//                      for larger pre-allocated frame buffer.
+if(psramFound()){  //�O�_��PSRAM(Psuedo SRAM)�O����IC
+  config.frame_size = FRAMESIZE_UXGA;
+  config.jpeg_quality = 10;
+  config.fb_count = 2;
+} else {
+  config.frame_size = FRAMESIZE_SVGA;
+  config.jpeg_quality = 12;
+  config.fb_count = 1;
+}
+//視訊初始化
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    ESP.restart();
+  }
+}
---
+++ CameraFaceRecognitionDoorLock/CameraFaceRecognitionDoorLock.ino
@@ -0,0 +1,89 @@
+/**********************************/
+/*以範例CAMERAWEBSERVER下去修改*/
+/*先給esp32cam餵人臉照片，最大number可到7個*/
+/*餵飽之後，辨識成功電磁鎖開鎖，失敗不理人*/
+/**********************************/
+
+#include "esp_camera.h"
+#include <WiFi.h>
+#define CAMERA_MODEL_AI_THINKER
+#define Relay 2
+#define Red 13
+#define Green 12
+#include "AI_THINKER.h"
+
+//密碼不要記起來!!//
+const char *ssid = "AndroidAP2775";
+const char *password = "mrtf7211";
+
+void startCameraServer();
+
+boolean matchFace = false;
+boolean activateRelay = false;
+long prevMillis = 0;
+int interval = 5000;
+
+void setup()
+{
+    pinMode(Relay, OUTPUT);
+    pinMode(Red, OUTPUT);
+    pinMode(Green, OUTPUT);
+    digitalWrite(Relay, LOW);
+    digitalWrite(Red, HIGH); //起始狀態
+    digitalWrite(Green, LOW);
+
+    Serial.begin(115200);
+    Serial.setDebugOutput(true);
+    Serial.println();
+
+    configSetup();
+
+    sensor_t *s = esp_camera_sensor_get();
+    // initial sensors are flipped vertically and colors are a bit saturated
+    if (s->id.PID == OV3660_PID)
+    {
+        s->set_vflip(s, 1);       // flip it back
+        s->set_brightness(s, 1);  // up the blightness just a bit
+        s->set_saturation(s, -2); // lower the saturation
+    }
+    // drop down frame size for higher initial frame rate
+    s->set_framesize(s, FRAMESIZE_QVGA);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("WiFi connected");
+
+    startCameraServer();
+
+    Serial.print("Camera Ready! Use 'http://");
+    Serial.print(WiFi.localIP());
+    Serial.println("' to connect");
+}
+
+void loop()
+{
+    if (matchFace == true && activateRelay == false)
+    {
+       //辨識成功則開綠燈//
+        activateRelay = true;
+        digitalWrite(Relay, HIGH);
+        digitalWrite(Green, HIGH);
+        digitalWrite(Red, LOW);
+        prevMillis = millis();
+    }
+    if (activateRelay == true && millis() - prevMillis > interval)
+    {
+        //辨識失敗紅燈不理人//
+        activateRelay = false;
+        matchFace = false;
+        digitalWrite(Relay, LOW);
+        digitalWrite(Green, LOW);
+        digitalWrite(Red, HIGH);
+    }
+}
