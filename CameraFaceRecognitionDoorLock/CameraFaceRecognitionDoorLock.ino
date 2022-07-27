5420b5478fbe98d94dfa738492a2f1d3028d2052
shawnyeh0308 <92806848+shawnyeh0308@users.noreply.github.com>
2022-07-27 23:22:48 +0800

    Update CameraFaceRecognitionDoorLock.ino

--- CameraFaceRecognitionDoorLock/CameraFaceRecognitionDoorLock.ino
+++ CameraFaceRecognitionDoorLock/CameraFaceRecognitionDoorLock.ino
@@ -1,7 +1,7 @@
 /**********************************/
 /*以範例CAMERAWEBSERVER下去修改*/
 /*先給esp32cam餵人臉照片，最大number可到7個*/
-/*餵飽之後，辨識成功電磁鎖開鎖，失敗不理人*/
+/*餵飽之後，如果辨識成功電磁鎖開鎖，失敗不理人*/
 /**********************************/
 
 #include "esp_camera.h"
@@ -16,7 +16,7 @@
 const char *ssid = "";
 const char *password = "";
 
-void startCameraServer(); //開啟WEBSERVER
+void startCameraServer(); //WEBSERVER函數
 
 boolean matchFace = false;
 boolean activateRelay = false;
@@ -24,12 +24,13 @@ long prevMillis = 0;
 int interval = 5000;
 
 void setup()
-{
+{   
+    //起始狀態,繼電器和Led
     pinMode(Relay, OUTPUT);
     pinMode(Red, OUTPUT);
     pinMode(Green, OUTPUT);
     digitalWrite(Relay, LOW);
-    digitalWrite(Red, HIGH); //起始狀態
+    digitalWrite(Red, HIGH); 
     digitalWrite(Green, LOW);
 
     Serial.begin(115200);
@@ -39,19 +40,23 @@ void setup()
     configSetup();
 
     sensor_t *s = esp_camera_sensor_get();
-    // initial sensors are flipped vertically and colors are a bit saturated
+    // 攝影鏡頭初始化
     if (s->id.PID == OV3660_PID)
     {
-        s->set_vflip(s, 1);       // flip it back
-        s->set_brightness(s, 1);  // up the blightness just a bit
-        s->set_saturation(s, -2); // lower the saturation
+        s->set_vflip(s, 1);       // 鏡頭翻轉
+        s->set_brightness(s, 1);  // 調高亮度
+        s->set_saturation(s, -2); // 降低飽和度
     }
-    // drop down frame size for higher initial frame rate
+    // 下拉幀數大小以獲得更高的初始幀數速率
     s->set_framesize(s, FRAMESIZE_QVGA);
+    //解析度 UXGA(1600x1200), SXGA(1280x1024), XGA(1024x768), SVGA(800x600), VGA(640x480), CIF(400x296)
+    //QVGA(320x240), HQVGA(240x176), QQVGA(160x120), QXGA(2048x1564 for OV3660)
+
 
     WiFi.begin(ssid, password);
+    //開始連接WIFI
 
-    while (WiFi.status() != WL_CONNECTED)
+    while (WiFi.status() != WL_CONNECTED) //連接未成功
     {
         delay(500);
         Serial.print(".");
@@ -59,7 +64,7 @@ void setup()
     Serial.println("");
     Serial.println("WiFi connected");
 
-    startCameraServer();
+    startCameraServer(); //開啟WEBSERVER
 
     Serial.print("Camera Ready! Use 'http://");
     Serial.print(WiFi.localIP()); //取得WEBSERVER的IP位址
@@ -70,7 +75,7 @@ void loop()
 {
     if (matchFace == true && activateRelay == false)
     {
-       //辨識成功則開綠燈//
+       //辨識成功則開綠燈，電磁鎖解鎖//
         activateRelay = true;
         digitalWrite(Relay, HIGH);
         digitalWrite(Green, HIGH);
